ca5a1f34d07ff19ca612bd210c501a25afb09c82
dmitry-sidorov <jimmaggot@mail.ru>
2026-04-03 19:50:55 +0300

    Fix unregister process

--- lib/chapter_ten/simple_registry.ex
+++ lib/chapter_ten/simple_registry.ex
@@ -45,13 +45,19 @@ defmodule ChapterTen.SimpleRegistry do
     {:reply, pid, storage}
   end
 
-  def handle_info({{:EXIT, pid, _reason}, storage}) do
+  @impl true
+  def handle_info({:EXIT, pid, _reason}, storage) do
     {:noreply, unregister_process(storage, pid)}
   end
 
   defp unregister_process(storage, unregistered_pid) do
-    storage
+    IO.inspect(storage, label: "storage on unregister")
+
+    new_storage = storage
     |> Enum.filter(fn {_key, process_pid} -> process_pid != unregistered_pid end)
     |> Enum.into(%{})
+
+    IO.inspect(new_storage, label: "new_storage on unregister")
+    new_storage
   end
 end
